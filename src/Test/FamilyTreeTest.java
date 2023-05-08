8790e70026a03426832d7f1715a09d80171583b0
dinhtung153 <127575856+dinhtung153@users.noreply.github.com>
2023-05-08 23:47:40 +0700

    Update FamilyTreeTest.java

--- src/Test/FamilyTreeTest.java
+++ src/Test/FamilyTreeTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Test;
 
 import javax.swing.UIManager;
@@ -15,21 +14,4 @@ public class FamilyTreeTest {
 		}
 	}
 }
-=======
-package Test;
 
-import javax.swing.UIManager;
-import View.FamilyTreeView;
-
-public class FamilyTreeTest {
-	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			FamilyTreeView frame = new FamilyTreeView();
-			frame.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
->>>>>>> f608c1f856143becc7ca6aa3ed6153852a471dff
